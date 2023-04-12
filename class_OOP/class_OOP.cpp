bb90b53bdde1c7f2f99d2e17dabcbe036d9b0db4
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-04-12 11:04:59 +0700

    menambahkan  member dengan hak akses private dan public

--- class_OOP/class_OOP.cpp
+++ class_OOP/class_OOP.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa {
-};
+class mahasiswa{
+private:
+	int nim;
+	string nama;
+	float nilai;
 
+public:
+	void input() {
+		cout << "masukan NIM :";
+		cin >> nim;
+		cout << "masukan Nama :";
+		cin >> nama;
+		cout << "masukan Nilai :";
+		cin >> nilai;
+	}
+	void printData();
+};
